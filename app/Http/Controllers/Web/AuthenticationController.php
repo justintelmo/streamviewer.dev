4518b829460e85ce1463a5baa4438501b969e91b
Psyballa <justin.telmo@gmail.com>
2018-10-12 12:39:04 -0700

    Put Redirect URI behind env variable to make it easier

--- app/Http/Controllers/Web/AuthenticationController.php
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -29,7 +29,7 @@ class AuthenticationController extends Controller
             $googleClient->setIncludeGrantedScopes(true);
             $googleClient->setAccessType('offline');
 
-            $googleClient->setRedirectUri('http://streamviewer-telmo.herokuapp.com/login/google/callback');
+            $googleClient->setRedirectUri(env("GOOGLE_REDIRECT_URI"));
 
             if ( isset ($_SESSION['access_token'] ) && $_SESSION['access_token'] ) 
             {
@@ -84,7 +84,7 @@ class AuthenticationController extends Controller
         $googleClient->addScope(Google_Service_People::USERINFO_EMAIL);
         $googleClient->setAccessType('offline');
         $googleClient->setIncludeGrantedScopes(true);
-        $googleClient->setRedirectUri('http://streamviewer-telmo.herokuapp.com/login/google/callback');
+        $googleClient->setRedirectUri(env("GOOGLE_REDIRECT_URI"));
 
         if ( ! isset( $_GET['code'] ) )
         {
