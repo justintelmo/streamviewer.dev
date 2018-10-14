baa8619f37be6f0d07974081dab1cbce991a281d
Psyballa <justin.telmo@gmail.com>
2018-10-14 04:05:34 -0700

    Add Middleware to proper spot in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CORS::class
     ];
 
     /**
@@ -37,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+            \App\Http\Middleware\CORS::class
         ],
 
         'api' => [
