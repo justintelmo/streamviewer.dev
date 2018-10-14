21d0c84342c8b64d6bb81e1d55a2884d65eed467
Psyballa <justin.telmo@gmail.com>
2018-10-14 03:56:40 -0700

    Re-make CORS middleware to solve problem..

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        return redirect('/', 302, [], true);
+        return route('login');
     }
 }
--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -13,23 +13,11 @@ class CORS {
      */
     public function handle($request, Closure $next)
     {
-
-        header("Access-Control-Allow-Origin: *");
-
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return Response::make('OK', 200, $headers);
+        if (!$request->secure() && App::environment() === "production") {
+            return redirect()->secure($request->getRequestUri());
         }
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return next($request);
     }
 
 }
